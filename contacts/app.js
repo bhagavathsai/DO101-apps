3346136dc843f75adb22da731589b0c09044c840
bhagavathsai <bhagavaths20@gmail.com>
2024-10-15 00:06:10 -0400

    add /mars endpoint

--- contacts/app.js
+++ contacts/app.js
@@ -19,6 +19,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
